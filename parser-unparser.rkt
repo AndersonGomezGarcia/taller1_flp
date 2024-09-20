8f0e771a068e50878944c8f73546fe8807cbe4f8
MaicolRodriguez12 <michael.arana@correounivalle.edu.co>
2024-09-20 15:16:03 -0500

    terminado parser y unparser

--- parser-unparser.rkt
+++ parser-unparser.rkt
@@ -1,4 +1,9 @@
 #lang eopl
+; Integrantes: 
+;Anderson Gomez -2266242
+;Nicolas Rodriguez - 2266071
+;Michael Rodriguez -2266193
+;Jhon Alexis Ruiz - 2266014
 
 (define-datatype circuito circuito?
     (cir_simple (c1 (list-of symbol?))
@@ -27,7 +32,7 @@
     (prim_xnor)
 )
 
-
+;Parser
 (define parser-circuito
     (lambda (list)
         (cond 
@@ -60,9 +65,36 @@
     )
 )
 
+; Unparser
+(define unparser-circuito
+  (lambda (ast)
+    (cases circuito ast
+      (cir_simple (c1 c2 ch) (list 'cir_simple c1 c2 (unparser-chip ch)))
+      (cir_comp (cir1 cir2 in out) (list 'cir_comp (unparser-circuito cir1) (map unparser-circuito cir2) in out)))))
+
+(define unparser-chip
+  (lambda (ast)
+    (cases chip ast
+      (prim_chip (cp) (list 'prim_chip (unparser-chip_prim cp)))
+      (comp_chip (c1 c2 cir) (list 'comp_chip c1 c2 (unparser-circuito cir))))))
+
+(define unparser-chip_prim
+  (lambda (ast)
+    (cases chip_prim ast
+      (prim_and () (list 'prim_and))
+      (prim_or () (list 'prim_or))
+      (prim_not () (list 'prim_not))
+      (prim_xor () (list 'prim_xor))
+      (prim_nand () (list 'prim_nand))
+      (prim_nor () (list 'prim_nor))
+      (prim_xnor () (list 'prim_xnor)))))
+
+
+
 ;Area del programador
 
-(define chip1
+;Listas
+(define chip1-list
   '(comp_chip
     (INA INB INC IND)
     (OUTA)
@@ -76,7 +108,7 @@
       (a b c d)
       (g))))
 
-(define circuito1
+(define cir1-list
   '(cir_comp
     (cir_simple (a b) (e)
       (prim_chip (prim_and)))
@@ -84,7 +116,85 @@
        (prim_chip (prim_and)))
      (cir_simple (e f) (g)
        (prim_chip (prim_or))))
-    (a b c d)
-    (g)))
+    (a b c d)
+    (g)))
 
+(define cir2-list
+  '(cir_comp
+    (cir_simple (m n o p) (e f)
+      (comp_chip
+        (INA INB INC IND)
+        (OUTE OUTF)
+        (cir_comp
+          (cir_simple (a b) (e)
+            (prim_chip (prim_and)))  
+          ((cir_simple (c d) (f)
+             (prim_chip (prim_and)))) 
+          (a b c d)
+          (e f))))
+    ((cir_simple (e f) (z)
+       (comp_chip
+         (INE INF)
+         (OUTA)
+         (cir_simple (e f) (g)
+           (prim_chip (prim_or)))))) 
+    (m n o p)
+    (z)))
+
+
+
+
+; AST
+(define chip1
+  (comp_chip
+  '(INA INB INC IND)
+  '(OUTA)
+  (cir_comp
+    (cir_simple '(a b) '(e)
+      (prim_chip (prim_and)))
+    (list
+      (cir_simple '(c d) '(f)
+        (prim_chip (prim_and)))
+      (cir_simple '(e f) '(g)
+        (prim_chip (prim_or))))
+  '(a b c d)
+  '(g))))
+
+(define cir1
+  (cir_comp
+    (cir_simple '(a b) '(e)
+      (prim_chip (prim_and)))
+    (list
+      (cir_simple '(c d) '(f)
+        (prim_chip (prim_and)))
+      (cir_simple '(e f) '(g)
+        (prim_chip (prim_or))))
+  '(a b c d)
+  '(g)
+  )
+)
+
+(define cir2
+  (cir_comp
+   (cir_simple '(m n o p) '(e f)
+    (comp_chip
+     '(INA INB INC IND)
+     '(OUTE OUTF)
+     (cir_comp
+      (cir_simple '(a b) '(e)
+        (prim_chip (prim_and)))  
+      (list
+       (cir_simple '(c d) '(f)
+         (prim_chip (prim_and))))  
+      '(a b c d)
+      '(e f))))
+   (list
+    (cir_simple '(e f) '(z)
+     (comp_chip
+      '(INE INF)
+      '(OUTA)
+      (cir_simple '(e f) '(g)
+        (prim_chip (prim_or))))))  
+   '(m n o p)
+   '(z)))
 
