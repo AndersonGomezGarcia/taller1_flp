61ab0954779abdfe668e8b8ad870845c41f6c78d
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2024-09-23 22:32:17 -0500

    ultimos ejemplos datatype

--- representacion-datatype.rkt
+++ representacion-datatype.rkt
@@ -31,32 +31,77 @@
     (prim_xor)
     (prim_xnor))
 
-
-(define chip1
-  (comp_chip
-  '(INA INB INC IND)
-  '(OUTA)
-  (cir_comp
-    (cir_simple '(a b) '(e)
-      (prim_chip (prim_and)))
-    (list
-      (cir_simple '(c d) '(f)
+(define cir1
+  (cir_comp 
+    (cir_simple '(A B) '(S1)
+      (prim_chip (prim_xor)))
+    (list 
+      (cir_simple '(S1 C) '(OUT1)
         (prim_chip (prim_and)))
-      (cir_simple '(e f) '(g)
-        (prim_chip (prim_or))))
-  '(a b c d)
-  '(g))))
+      (cir_simple '(S1 D) '(OUT2)
+        (prim_chip (prim_or)))
+    )
+    '(A B C D)
+    '(OUT1 OUT2)
+  ))
 
-(define circuito1
-  (cir_comp
-    (cir_simple '(a b) '(e)
+
+(define cir2
+  (cir_comp 
+    (cir_simple '(A B) '(X)
       (prim_chip (prim_and)))
-    (list
-      (cir_simple '(c d) '(f)
+    (list 
+      (cir_simple '(C D) '(Y)
+        (prim_chip (prim_or)))
+      (cir_simple '(X Y) '(OUT1)
+        (prim_chip (prim_nand)))
+    )
+    '(A B C D)
+    '(OUT1)
+  ))
+
+(define cir3
+  (cir_comp 
+    (cir_simple '(A B) '(S1)
+      (prim_chip (prim_xor)))
+    (list 
+      (cir_simple '(S1 C) '(OUT1)
         (prim_chip (prim_and)))
-      (cir_simple '(e f) '(g)
-        (prim_chip (prim_or))))
-  '(a b c d)
-  '(g)
-  )
-)
+      (cir_simple '(C D) '(OUT2)
+        (prim_chip (prim_or)))
+    )
+    '(A B C D)
+    '(OUT1 OUT2)
+  ))
+
+  (define cir_comp1
+  (cir_comp 
+    (cir_simple '(A B C) '(S1)
+      (prim_chip (prim_nor)))
+    (list 
+      (cir_simple '(S1 D) '(OUT1)
+        (prim_chip (prim_xnor)))
+      (cir_simple '(C D) '(S2)
+        (prim_chip (prim_and)))
+      (cir_simple '(S2 S1) '(OUT2)
+        (prim_chip (prim_or)))
+    )
+    '(A B C D)
+    '(OUT1 OUT2)
+  ))
+
+(define cir_comp2
+  (cir_comp 
+    (cir_simple '(A B C) '(S1)
+      (prim_chip (prim_and)))
+    (list 
+      (cir_simple '(S1 D) '(OUT1)
+        (prim_chip (prim_xor)))
+      (cir_simple '(C D) '(S2)
+        (prim_chip (prim_not)))
+      (cir_simple '(S2 E) '(OUT2)
+        (prim_chip (prim_or)))
+    )
+    '(A B C D E)
+    '(OUT1 OUT2)
+  ))
